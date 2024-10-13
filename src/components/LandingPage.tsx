609267b093fdc788820c1da7d8a73452dc0cf1a0
iamdnltngl <tongoldanieljoshua24@gmail.com>
2024-10-13 20:08:51 +0800

    added design to the content

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,8 +1,11 @@
 import illustrationDesktop from "../assets/images/illustration-sign-up-desktop.svg"
 import illustrationMobile from "../assets/images/illustration-sign-up-mobile.svg"
+import iconList from "../assets/images/icon-list.svg"
+import iconSuccess from "../assets/images/icon-success.svg"
+
 function LandingPage(){
     return (
-      <section>
+      <section className=" font-Roboto">
         <div className="sm:flex">
           <picture className="order-2 sm:order-1 sm:max-h-fit  md:flex ">
             <source
@@ -17,25 +20,39 @@ function LandingPage(){
             <h1 className="text-4xl font-Roboto font-bold mt-3">
               Stay updated!
             </h1>
-            <p className="text-base font-RobotoRegular text-neutral-DarkSlateGrey">
+            <p className="text-base font-RobotoRegular text-neutral-DarkSlateGrey mt-4">
               Join 60,000+ product managers receiving monthly updates on:
             </p>
-            <ul>
-              <li>
-                Product discovery and building what matters And much more!
-              </li>
-              <li>Measuring to ensure updates are a success </li>
-              <li>Measuring to ensure updates are a success</li>
+            <ul className="list">
+              <div className="flex gap-5">
+                <img className="  " src={iconList} alt="" />
+                <li className="mt-4">
+                  Product discovery and building what matters And much more!
+                </li>
+              </div>
+              <div className="flex gap-5">
+                <img className="  " src={iconList} alt="" />
+                <li className="mt-4">
+                  Measuring to ensure updates are a success
+                </li>
+              </div>
+              <div className="flex gap-5">
+                <img className="  " src={iconList} alt="" />
+                <li className="mt-4">
+                  Measuring to ensure updates are a success
+                </li>
+              </div>
             </ul>
-            <form action="">
+            <form className="mt-7" action="">
+                <h2 className="text-sm mb-2 font-semibold ">Email address</h2>
               <label htmlFor="Email address"></label>
               <input
-                className="outline"
+                className="outline p-2 w-full rounded-lg outline-neutral-Grey"
                 type="email"
                 name="email"
                 placeholder="email@company.com"
               />
-              <button type="submit">Subscribe to monthly newsletter</button>
+              <button className="submitButton w-full outline p-3 rounded-lg mt-5 bg-neutral-DarkSlateGrey text-neutral-White font-Roboto font-bold text-sm" type="submit">Subscribe to monthly newsletter</button>
             </form>
           </div>
         </div>
